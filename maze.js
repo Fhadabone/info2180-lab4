9d056e46561b0c165b269c6dfcb0a22e5d155c5e
Fhadabone <44045828+Fhadabone@users.noreply.github.com>
2018-10-22 21:41:36 -0500

    part2
    all walls will now be red

--- maze.js
+++ maze.js
@@ -3,11 +3,41 @@ window.onload = function(){
 };
 
 function main(){
+	//**************************************************************
+
 	//part1
 	let wall = document.getElementById('boundary1');
-	wall.onmouseover=function(){
+	wall.onmouseover = function(){
 		this.setAttribute('class', 'boundary youlose');
 
 	};
+
+	//**************************************************************
+
 	
+
+	//part2 V1
+	var fails=0;
+	let walls = document.querySelectorAll('.boundary');
+	walls.forEach(function(elem){
+		elem.onmouseover = function(){
+			fails++;
+			this.setAttribute('class', 'boundary youlose');
+		};
+
+	});
+	//****************************************************************
+
+	
+	
+
+
+	
+
+	
+
+	
+
+
+		
 };
