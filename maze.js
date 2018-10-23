468f19d8b1f253bb65c6a648aae1cceee187b95a
Fhadabone <44045828+Fhadabone@users.noreply.github.com>
2018-10-23 17:36:38 -0500

    Commit Part2

--- maze.js
+++ maze.js
@@ -5,16 +5,29 @@ window.onload = function(){
 	main();
 	
 };
-
-
 function main(){
 	started=true;
-	if(started){
+	/*if(started){
 		let wall = document.getElementById('boundary1');
 		wall.onmouseover = function(){
 			this.setAttribute('class', 'boundary youlose');
 		};
 	};
+	*/
+
+	let wall = 0;
+	let i = 0;
+	let walls = document.querySelectorAll('.boundary');
+	if(started){
+		for(i = 0; i < walls.length; i++){
+			walls[i].onmouseover = function(){
+				fails=true;
+				for(wall = 0; wall < walls.length-1; wall++){
+					walls[wall].setAttribute('class','boundary youlose');
+				};
+			};
+		};
+	}
 	
 
 
